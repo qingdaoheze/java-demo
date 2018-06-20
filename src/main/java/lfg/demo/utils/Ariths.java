f6cdb7c97759aab7d7d627d47d03625cc3ae233b
Li FengGang <lifenggang@163.com>
2018-06-20 16:35:31 +0800

    format

--- src/main/java/lfg/demo/utils/Ariths.java
+++ src/main/java/lfg/demo/utils/Ariths.java
@@ -10,9 +10,12 @@ import java.math.BigDecimal;
  */
 public class Ariths {
 	/**
-	 * 功能：BigDecimal类型的加法运算。 
-	 * @param d1 被加数
-	 * @param d2 加数
+	 * 功能：BigDecimal类型的加法运算。
+	 * 
+	 * @param d1
+	 *            被加数
+	 * @param d2
+	 *            加数
 	 * @return 两个参数的和
 	 */
 	public static final double add(double d1, double d2) {
@@ -27,7 +30,7 @@ public class Ariths {
 	 * @param ds
 	 * @return
 	 */
-	public static final double add(double...ds) {
+	public static final double add(double... ds) {
 		BigDecimal total = BigDecimal.ZERO;
 		for (double d0 : ds) {
 			total.add(new BigDecimal(Double.toString(d0)));
@@ -38,8 +41,10 @@ public class Ariths {
 	/**
 	 * BigDecimal类型的减法运算。
 	 * 
-	 * @param d1 被减数
-	 * @param d2 减数
+	 * @param d1
+	 *            被减数
+	 * @param d2
+	 *            减数
 	 * @return 两个参数的差
 	 */
 	public static final double subtract(double d1, double d2) {
@@ -51,8 +56,10 @@ public class Ariths {
 	/**
 	 * 两个数的乘法
 	 * 
-	 * @param d1 被乘数
-	 * @param d2 乘数
+	 * @param d1
+	 *            被乘数
+	 * @param d2
+	 *            乘数
 	 * @return 乘积
 	 */
 	public static final double multiply(double d1, double d2) {
@@ -64,9 +71,12 @@ public class Ariths {
 	/**
 	 * 两个数的除法运算，当除不尽的时候，由scale指定精度，做四舍五入。
 	 * 
-	 * @param d1 被除数
-	 * @param d2 除数
-	 * @param scale 结果的精度，即保留几位小数
+	 * @param d1
+	 *            被除数
+	 * @param d2
+	 *            除数
+	 * @param scale
+	 *            结果的精度，即保留几位小数
 	 * @return 商
 	 */
 	public static final double divide(double d1, double d2, int scale) {
@@ -81,9 +91,12 @@ public class Ariths {
 	/**
 	 * 两个数的除法运算，当除不尽的时候，由scale指定精度，向远离0的方向舍入。
 	 * 
-	 * @param d1 被除数
-	 * @param d2 除数
-	 * @param scale 结果的精度，即保留几位小数
+	 * @param d1
+	 *            被除数
+	 * @param d2
+	 *            除数
+	 * @param scale
+	 *            结果的精度，即保留几位小数
 	 * @return 商
 	 */
 	public static final double divideUp(double d1, double d2, int scale) {
@@ -98,9 +111,12 @@ public class Ariths {
 	/**
 	 * 两个数的除法运算，当除不尽的时候，由scale指定精度，向接近0的方向舍入。
 	 * 
-	 * @param d1 被除数
-	 * @param d2 除数
-	 * @param scale 结果的精度，即保留几位小数
+	 * @param d1
+	 *            被除数
+	 * @param d2
+	 *            除数
+	 * @param scale
+	 *            结果的精度，即保留几位小数
 	 * @return 商
 	 */
 	public static final double divideDown(double d1, double d2, int scale) {
@@ -115,9 +131,12 @@ public class Ariths {
 	/**
 	 * 两个数的除法运算，当除不尽的时候，由scale指定精度，向正无穷的方向舍入。
 	 * 
-	 * @param d1 被除数
-	 * @param d2 除数
-	 * @param scale 结果的精度，即保留几位小数
+	 * @param d1
+	 *            被除数
+	 * @param d2
+	 *            除数
+	 * @param scale
+	 *            结果的精度，即保留几位小数
 	 * @return 商
 	 */
 	public static final double divideCeiling(double d1, double d2, int scale) {
@@ -132,9 +151,12 @@ public class Ariths {
 	/**
 	 * 两个数的除法运算，当除不尽的时候，由scale指定精度，向负无穷的方向舍入。
 	 * 
-	 * @param d1 被除数
-	 * @param d2 除数
-	 * @param scale 结果的精度，即保留几位小数
+	 * @param d1
+	 *            被除数
+	 * @param d2
+	 *            除数
+	 * @param scale
+	 *            结果的精度，即保留几位小数
 	 * @return 商
 	 */
 	public static final double divideFloor(double d1, double d2, int scale) {
@@ -146,5 +168,4 @@ public class Ariths {
 		return oper1.divide(oper2, scale, BigDecimal.ROUND_FLOOR).doubleValue();
 	}
 
-	
 }
